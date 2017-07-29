a98f803b2518bfe9f04467ad20759167905046bf
logan <logan@DESKTOP-D5N2P9T>
2017-07-29 15:59:20 +1200

    step2

--- Duckz/Duckz/AzureTable.xaml.cs
+++ Duckz/Duckz/AzureTable.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Duckz
 {
-    public partial class MainPage : TabbedPage
+
+    public partial class AzureTable : ContentPage
     {
-        public MainPage()
+        public AzureTable()
         {
             InitializeComponent();
         }
